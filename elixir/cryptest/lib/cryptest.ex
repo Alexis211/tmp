af377ad06f3c2eb299f57bc0fbc2c00541e15b17
Alex Auvolat <alex.auvolat-bernstein@inria.fr>
2018-06-25 13:26:39 +0200

    start elixir project

---
+++ elixir/cryptest/lib/cryptest.ex
@@ -0,0 +1,18 @@
+defmodule Cryptest do
+  @moduledoc """
+  Documentation for Cryptest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cryptest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/cryptest/test/cryptest_test.exs
@@ -0,0 +1,8 @@
+defmodule CryptestTest do
+  use ExUnit.Case
+  doctest Cryptest
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
